bd12bbf68f853475b4cfa3d11cd7b5481515ca97
Krishan <krishangoyal9350@gmail.com>
2023-08-12 15:58:41 +0530

    homepage design

---
+++ src/components/countdown.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Countdown from "react-countdown";
+
+const endingDate = new Date("2023-08-13");
+
+const CountDown = () => {
+  return (
+    <Countdown
+      date={endingDate}
+      className="text-5xl font-bold text-yellow-300"
+    />
+  );
+};
+
+export default CountDown;
